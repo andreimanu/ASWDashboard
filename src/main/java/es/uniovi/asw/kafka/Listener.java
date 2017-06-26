ab9e3ee1932015dd16d0ae4acaf61e50e0e5acb6
Christian Martz <uo237441@uniovi.es>
2017-06-26 04:47:34 +0200

    listener shows messages data

--- src/main/java/es/uniovi/asw/kafka/Listener.java
+++ src/main/java/es/uniovi/asw/kafka/Listener.java
@@ -14,23 +14,33 @@ public class Listener {
 	public void listen(ConsumerRecord<?, ?> record) {
 		MainController.refresh = true;
 		System.out.println("------------------\n RECEIVED MESSAGE VOTED PROPOSAL \n -------------------");
+		System.out.println("Proposal ID: " + record.value().toString().split(":")[0] + ", from user: " 
+				+ record.value().toString().split(":")[1] + " and is: " +
+				("1".equals(record.value().toString().split(":")[2])? "positive":"negative"));
 	}
 
 	@KafkaListener(id = "vcListener", topics = "votedComment", group = "test")
 	public void listenVotedComment(ConsumerRecord<?, ?> record) {
 		MainController.refresh = true;
 		System.out.println("------------------\n RECEIVED MESSAGE VOTED COMMENT \n -------------------");
+		System.out.println("Comment ID: " + record.value().toString().split(":")[0] + ", from user: " 
+				+ record.value().toString().split(":")[1] + " and is: " +
+				("1".equals(record.value().toString().split(":")[2])? "positive":"negative"));
 	}
 	
 	@KafkaListener(id = "ccListener", topics = "createdComment", group = "test")
 	public void listenCreatedComment(ConsumerRecord<?, ?> record) {
 		MainController.refresh = true;
 		System.out.println("------------------\n RECEIVED MESSAGE CREATED COMMENT \n -------------------");
+		System.out.println("new Comment ID: " + record.value().toString().split(":")[0] + ", from user: " 
+				+ record.value().toString().split(":")[1]);
 	}
 	
 	@KafkaListener(id = "cpListener", topics = "createdProposal", group = "test")
 	public void listenCreatedProposal(ConsumerRecord<?, ?> record) {
 		MainController.refresh = true;
 		System.out.println("------------------\n RECEIVED MESSAGE CREATED PROPOSAL \n -------------------");
+		System.out.println("new Proposal ID: " + record.value().toString().split(":")[0] + ", from user: " 
+				+ record.value().toString().split(":")[1]);
 	}
 }
